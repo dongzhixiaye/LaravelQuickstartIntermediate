72fe4d86f707aa94080099fa6793b55589685121
Tim F <tfarmer4@gmail.com>
2016-02-23 20:29:41 -0600

    copy TaskController from gitlab repo for the tutorial in case mine is causing the redirect loop

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,68 +1,68 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Task;
 use App\Repositories\TaskRepository;
-
 class TaskController extends Controller
-{	
-	/**
-	 * The task repository instance.
-	 * @var TaskRepository
-	 */
-	protected $tasks;
-
-
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return  void
-	 */
+{
+    /**
+     * The task repository instance.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+    /**
+     * Create a new controller instance.
+     *
+     * @param  TaskRepository  $tasks
+     * @return void
+     */
     public function __construct(TaskRepository $tasks)
     {
-    	//force authentication
-    	$this->middleware('auth');
-
-    	$this->tasks = $tasks;
+        $this->middleware('auth');
+        $this->tasks = $tasks;
     }
-
     /**
-     * Display a list of all the user's tasks.
-     * 
-     * @param  Request $request 
+     * Display a list of all of the user's task.
+     *
+     * @param  Request  $request
      * @return Response
      */
     public function index(Request $request)
     {
-    	return view('tasks.index', [
-    		'tasks'=>$this->tasks->forUser($request->user()),
-    	]);
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
     }
-
+    /**
+     * Create a new task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
     public function store(Request $request)
     {
-    	$this->validate($request, [
-    		'name'=>'required|max:255',
-    	]);
-
-    	//Create the Task...
-    	$request->user()->tasks()->create([
-    		'name'=>$request->name,
-    	]);
-
-    	return redirect('/tasks');
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+        return redirect('/tasks');
     }
-
-    public funtion destroy(Request $request, Task $task)
+    /**
+     * Destroy the given task.
+     *
+     * @param  Request  $request
+     * @param  Task  $task
+     * @return Response
+     */
+    public function destroy(Request $request, Task $task)
     {
-    	$this->authorize('destroy', $task);
-
-    	$task->delete();
-
-    	return redirect('/tasks');
+        $this->authorize('destroy', $task);
+        $task->delete();
+        return redirect('/tasks');
     }
 }

f2d31197b8f312b8aa0b91456fc3037c2cb775b1
Tim F <tfarmer4@gmail.com>
2016-02-23 20:04:29 -0600

    The rest of the tutorial

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,17 +6,65 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
-{
+{	
+	/**
+	 * The task repository instance.
+	 * @var TaskRepository
+	 */
+	protected $tasks;
+
 
 	/**
 	 * Create a new controller instance.
 	 *
 	 * @return  void
 	 */
-    public function __construct()
+    public function __construct(TaskRepository $tasks)
     {
+    	//force authentication
     	$this->middleware('auth');
+
+    	$this->tasks = $tasks;
+    }
+
+    /**
+     * Display a list of all the user's tasks.
+     * 
+     * @param  Request $request 
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+    	return view('tasks.index', [
+    		'tasks'=>$tasks,
+    	]);
+    }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		'name'=>'required|max:255',
+    	]);
+
+    	//Create the Task...
+    	$request->user()->tasks()->create([
+    		'name'=>$request->name,
+    	]);
+
+    	return redirect('/tasks');
+    }
+
+    public funtion destroy(Request $request, Task $task)
+    {
+    	$this->authorize('destroy', $task);
+
+    	$task->delete();
+
+    	return redirect('/tasks');
     }
 }
